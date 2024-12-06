93dfd38096827e36f41b7fa468109949fa7be19e
MTVFILMDIRECT <namal005perera@gmail.com>
2024-12-06 22:38:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "please enter your session code 😊😊",
+SESSION_ID: process.env.SESSION_ID || "q3IyDQAa#Ubady82nNXn_mhdRi-fZBaS3lRM6kusGHI27Jttz1gI",
 MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_5278.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
